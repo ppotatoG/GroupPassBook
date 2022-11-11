95b1f45f476512767ecacbead4eb02510e6ed9fa
ppotatoG <thgus2270@gmail.com>
2022-11-11 11:24:22 +0900

    Initial setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    webpack(config, {webpack}) {
+        config.resolve = {
+            alias: {
+                '@components': path.resolve(__dirname, './components'),
+                '@styles': path.resolve(__dirname, './styles'),
+            },
+            ...config.resolve
+        };
+        return config;
+    }
 }
 
 module.exports = nextConfig
